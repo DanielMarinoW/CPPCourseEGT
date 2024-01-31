85b79f73e6b5b8cac9f1986eb3dc2e471836d8fe
DanielMarinoW <daniel2001_1973@abv.bg>
2024-01-31 17:50:06 +0200

    switch case

---
+++ Switch case/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	
+	switch (a) {
+	case 1: cout << "Ponedelnik"; break;
+	case 2: cout << "Vtornik"; break;
+	case 3: cout <<"Srqda"; break;
+	case 4: cout << "Chtvurtk"; break;
+	case 5: cout <<"petak"; break;
+	case 6: cout << "subota"; break;
+	case 7: cout <<"nedelq"; break;
+	default: cout << "Ne e den ot semicata0";
+
+
+
+	}
+	
+	
+	
+}
+
