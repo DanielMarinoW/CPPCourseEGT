30c258ded99928daaf6291169bcbf743b19a092f
DanielMarinoW <157685635+DanielMarinoW@users.noreply.github.com>
2024-02-07 20:52:37 +0200

    Update Task.h

--- LibraryManagementSystem_Homework/Task.h
+++ LibraryManagementSystem_Homework/Task.h
@@ -1,5 +1,13 @@
 #pragma once
-class Task
-{
-};
+
+
+//Implementing a Library Management System
+	//Task Description
+	//Creat a C++ program for a simple library management system.
+	//The program should include classes for books and library members, and demonstrate
+	//the use of objects,methods,and pointers.
+	
+	//Implement the classes as described above.
+	//in the main function, creat at least two Book objects and two Member object.
+
 
